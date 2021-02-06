2c4e8ab35ee23b5b6bce1c51d6470e13b3a0d11b
Aleksandr Gorokhov <gorochov.as@gmail.com>
2021-02-06 15:00:58 +0200

    Create pages

---
+++ app/admin/weather_sources.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register WeatherSource do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :url, :css_selector
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :url, :css_selector]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
